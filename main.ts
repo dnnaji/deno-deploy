df1a981e2fb252e435799560cadb3d341edf6598
Derrick Nnaji <dnnaji@gmail.com>
2025-02-22 21:20:10 -0600

    add Hono app

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-Deno.serve((req) => new Response("Hello, Deno Deploy!"));
+import { Hono } from "hono";
+import { Context } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c: Context) => c.text("Hello, Hono!"));
+
+// Start the server
+Deno.serve(app.fetch);
